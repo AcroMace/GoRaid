d9e7dae55c24890faf5f07b29975a19039606c89
Andy Cho <hello@andycho.ca>
2020-08-10 19:28:22 -0700

    Lint

--- GoRaid/Pokemon.swift
+++ GoRaid/Pokemon.swift
@@ -46,18 +46,18 @@ struct Pokemon: Hashable, Codable, Identifiable {
     static func loadPokemon() -> [Pokemon] {
         let filename = "pokedex.json"
         let data: Data
-        
+
         guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
             else {
                 fatalError("Couldn't find \(filename) in main bundle.")
         }
-        
+
         do {
             data = try Data(contentsOf: file)
         } catch {
             fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
         }
-        
+
         do {
             let decoder = JSONDecoder()
             return try decoder.decode([Pokemon].self, from: data)
