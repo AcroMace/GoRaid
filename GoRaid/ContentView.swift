63993258fcabbee10dd1e95fa9228d74ae565d74
Andy Cho <hello@andycho.ca>
2020-08-10 17:43:11 -0700

    Initial Commit

---
+++ GoRaid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoRaid
+//
+//  Created by Andy Cho on 2020-08-10.
+//  Copyright © 2020 AcroMace. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
